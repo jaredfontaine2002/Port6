3216564f3a378e21f5037db7747076d2052a7935
Jared <jaredfontaine2002@gmail.com>
2021-03-17 12:09:37 -0600

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'I am a data analyst specializing in Python and Jupyter Notebooks.  I started my career in data  while working as the field director for a United States Congressman David McKinley WV-01 using Microsoft Excel and Access to query targeted voter lists canvasing lists for campaign volunteers from the voter registration lists. These lists allowed us to target soft voters with certain persuasive messages.   '}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    '  During studies at Wheeling Jesuit University, I worked as a data analyst for the University’s marketing department conducting forensic research using Microsoft Access and Excel. '}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || ' I entered data from the old marketing department’s POs to create a dashboard that was used for the University’s Performance metrics.  '}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,86 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Jared Fontaine', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Jared Fontaine',
+  subtitle: "I'm the data analyst cyclist",
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'Fontaine_Profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://resume.io/r/kpko1cs5N', // if no resume, the button will not show up
+}; 
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'Two_bears.jpg',
+    title: 'Human Deaths Caused by Bears',
+    info: "The first project I have is a bear-human deaths data visualization.  I used MapQuest's free API to get the latitudes and longitudes of all 166-bear mauling's in the United States and plot them on an interactive map.  I presented this project to the Code for the Web meetup group.  I used Pandas, Plotly, Dash and matplotlib and a few other packages to create the visualization.  Dash is a great resource that allows the developer to create and publish online interactive data visualizations with only Python.  The Dash is built on top of React, JavaScript, and other languages so that the developer can focus on creating amazing visuals. I lead the group discussion and demonstrated my proficiencies with the technologies described above to the group.       ",
+    info2: '',
+    
+    repo: 'https://github.com/jaredfontaine2002/Bear_Attack', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Bear_Trash.jpg',
+    title: 'Nuisance Bears Data Visualization',
+    info: "This project expands on the first project.  The first project analyzed only bear deaths.  This project analyzes all negative bear-human interacts.  I used the Federal Freedom of Information act to gather all the bear-human negative interactions from the States of Colorado, and Alaska recorded by their respective department of wildlife.  I want to see if there are any linear correlations between bear-human interactions and depletion of food sources, climate change population density increases, and food storages etc. ",
+    info2: "I am using a linear regression and maybe a logistic regression on the data.  I have collected information from Colorado and Alaska for the last 2 years.  Many states have not recorded their bear-human interactions in a digital usable format in the past so this is truly ground break information.  The project is ongoing and I will all more states as their information comes in",
+    
+    repo: 'https://github.com/jaredfontaine2002/Colorado_bear', // if no repo, the button will not show up
+  },
+  
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'jaredfontaine2002@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/jared-fontaine-71116116/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/jaredfontaine2002',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
